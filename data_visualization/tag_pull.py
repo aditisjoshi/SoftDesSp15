3c761009a4d80b8b5ec28ab22d09d11725269368
jsutantio <jessica.sutantio@students.olin.edu>
2015-03-05 12:52:41 -0500

    Setup the hastags with the various permutations for theDress.

---
+++ data_visualization/tag_pull.py
@@ -0,0 +1,87 @@
+## Aditi Joshi & Jessica Sutantio
+## Software Design SPring 2015 Mini Project 4
+
+"""
+Searches the social media sites: Twitter, Tumblr, Facebook, and Instagram
+for tags that are relevant to the topic of the color of the viral laced dress.
+From these tags, we will find the popular opinion of the dress' color and 
+enable the user to view which opinion over time.
+
+"""
+
+## Hashtag permutations
+	"""
+	'blackandblue'
+	'blacknblue'
+	'blackblue'
+	'blueandblack'
+	'bluenblack'
+	'blueblack'
+
+	'whiteandgold'
+	'whitengold'
+	'whitegold'
+	'goldandwhite'
+	'goldnwhite'
+	'goldwhite'
+
+	all of the above + 'dress'
+
+	'thedress'
+	"""
+
+theDresstags = ['blackandblue','blacknblue','blackblue','blueandblack','bluenblack',
+				'blueblack','whiteandgold','whitengold','whitegold','goldandwhite',
+				'goldnwhite','goldwhite']
+
+def string2hashtag(strlist):
+	# add the hastag symbol to the list of strings
+	theDresshashtags = []
+	for tag in strlist:
+		tag = '#' + tag
+		# add 'dress' to each hashtag
+		dresstag = tag + 'dress'
+		theDresshashtags.append(tag)
+		theDresshashtags.append(dresstag)
+	theDresshashtags.append('#thedress')
+
+	return theDresshashtags
+
+print string2hashtag(theDresstags)
+
+
+
+"""Twitter streaming"""
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "ENTER YOUR ACCESS TOKEN"
+access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
+consumer_key = "ENTER YOUR API KEY"
+consumer_secret = "ENTER YOUR API SECRET"
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
